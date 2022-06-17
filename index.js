0d246f0bd1e7b2cf45677849381930cd0b0b7f47
Jacqueline Mckay H <jacquelinemc@outlook.es>
2022-06-16 19:27:34 -0500

    feat: first challenge, modal and button

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-// Busquemos el formulario en el DOM
+const initialCards = [{
+        name: "Valle de Yosemite",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lago Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Montañas Calvas",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Parque Nacional de la Vanoise",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
+
+const cardTemplate = document.querySelector("#elements").textContent;
+const cardsContainer = document.querySelector(".elements__element");
+
+const addElement = function() {
+    initialCards.forEach(function(item) {
+        const cardElement = cardTemplate.querySelector(".elements__element").cloneNode(true);
+
+        cardElement.querySelector(".elements__place").textContent = item.name;
+        cardElement.querySelector(".elements__photo").src = item.link;
+        cardElement.querySelector(".elements__photo").alt = item.name;
+    });
+};
+
+
 let formElement = document.querySelector('.popup__form');
 let formButton = document.querySelector('.popup__button-form');
+let profileButton = document.querySelector('.profile__btn');
+
 
 function handleProfileFormSubmit(evt) {
 
@@ -17,6 +58,7 @@ function handleProfileFormSubmit(evt) {
 
 }
 
+
 function openForm() {
     document.querySelector(".popup").style.display = "block";
 }
@@ -25,6 +67,17 @@ function closeForm() {
     document.querySelector(".popup").style.display = "none";
 }
 
+
+function openFormImages() {
+    document.querySelector(".form").style.display = "block";
+}
+
+function closeFormImages() {
+    document.querySelector(".form").style.display = "none";
+}
+
 document.querySelector('.profile__button-person').addEventListener("click", openForm)
 document.querySelector('.popup__button-close').addEventListener("click", closeForm)
 document.querySelector(".popup__button-form").addEventListener("click", handleProfileFormSubmit)
+document.querySelector('.profile__btn').addEventListener("click", openFormImages)
+document.querySelector('.form__button').addEventListener("click", closeFormImages)
