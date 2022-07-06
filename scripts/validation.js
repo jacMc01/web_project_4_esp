0d32639d620b6a19102ee7f4b6b9470dbe003233
Jacqueline Mckay H <jacquelinemc@outlook.es>
2022-07-06 11:31:30 -0500

    feat: validations forms

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,36 +1,150 @@
-// Primero, selecciona todos los elementos necesarios del formulario, y asígnalos a variables
-const formElement = document.querySelector(".popup__form");
-const formInput = formElement.querySelector(".popup__name");
+const showError = (form, inputElement, errorMessage) => {
+    const errorElement = form.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add("popup__valid");
+    errorElement.textContent = errorMessage;
+    errorElement.classList.remove("popup__active-error")
+}
 
+const hideInputError = (form, inputElement) => {
+    const errorElement = form.querySelector(`.${inputElement.id}-error`);
 
-// Escribe el código de la primera función, que muestra el elemento erróneo
-const showInputError = (element) => {
-    element.classList.add("popup__name-error");
-    element.classList.add("popup__about-error");
+    inputElement.classList.remove("popup__valid");
+    errorElement.classList.add("popup__active-error");
+    errorElement.textContent = "";
 };
 
-// Escribe el código de la segunda función, que oculta el elemento erróneo
-const hideInputError = (element) => {
-    element.classList.remove("popup__name-error");
-    element.classList.remove("popup__about-error");
-};
-
-// Escribe el código de la tercera función, que comprueba si el campo es válido
-const isValid = () => {
-    if (!formInput.validity.valid) {
-        // Si NO lo es (!), muestra el elemento erróneo
-        showInputError(formInput);
+const checkInputValidity = (form, inputElement) => {
+    if (!inputElement.validity.valid) {
+        showError(form, inputElement, inputElement.validationMessage);
     } else {
-        // Si es válido, oculta el elemento erróneo
-        hideInputError(formInput);
+        hideInputError(form, inputElement);
     }
 };
 
-formElement.addEventListener("submit", function(evt) {
-    // Cancela la acción del navegador por defecto, de modo que al hacer clic en el botón "Enviar" no se actualice la página
-    evt.preventDefault();
-    console.log("Formulario enviado");
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    });
+};
+
+const toggle = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add("popup__button-form_inactive");
+        buttonElement.disabled = true;
+    } else {
+        buttonElement.classList.remove("popup__button-form_inactive");
+        buttonElement.disabled = false;
+    }
+};
+
+const setEventListeners = form => {
+    const inputList = Array.from(form.querySelectorAll(".popup__input"));
+    const buttonElement = form.querySelector(".popup__button-form");
+
+    toggle(inputList, buttonElement);
+
+    inputList.forEach(function(inputElement) {
+        inputElement.addEventListener("input", function() {
+            checkInputValidity(form, inputElement);
+
+            toggle(inputList, buttonElement);
+        });
+    });
+};
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll(".popup__form"));
+
+    formList.forEach((form) => {
+        form.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+        });
+        setEventListeners(form);
+    });
+};
+
+enableValidation({
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".popup__button-form",
+    inactiveButtonClass: "popup__button-form_inactive",
+    inputErrorClass: "popup__active-error",
+    errorClass: "popup__valid"
 });
 
-// Llama a la función isValid() para cada entrada de caracteres
-formInput.addEventListener("Input se ejecuta", isValid);
+/*######################################################################################*/
+
+
+const showErrorForm2 = (form, inputElement, errorMessage) => {
+    const errorElement = form.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add("form__valid");
+    errorElement.textContent = errorMessage;
+    errorElement.classList.remove("form__active-error")
+}
+
+const hideInputErrorForm2 = (form, inputElement) => {
+    const errorElement = form.querySelector(`.${inputElement.id}-invalid`);
+
+    inputElement.classList.remove("form__valid");
+    errorElement.classList.add("form__active-error");
+    errorElement.textContent = "";
+};
+
+const checkInputValidityForm2 = (form, inputElement) => {
+    if (!inputElement.validity.valid) {
+        showErrorForm2(form, inputElement, inputElement.validationMessage);
+    } else {
+        hideInputErrorForm2(form, inputElement);
+    }
+};
+
+const hasInvalidInputForm2 = (inputList) => {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    });
+};
+
+const toggleForm2 = (inputList, buttonElement) => {
+    if (hasInvalidInputForm2(inputList)) {
+        buttonElement.classList.add("form__button-form_inactive");
+        buttonElement.disabled = true;
+    } else {
+        buttonElement.classList.remove("form__button-form_inactive");
+        buttonElement.disabled = false;
+    }
+};
+
+const setEventListenersForm2 = form => {
+    const inputList = Array.from(form.querySelectorAll(".form__input"));
+    const buttonElement = form.querySelector(".form__button-form");
+
+    toggleForm2(inputList, buttonElement);
+
+    inputList.forEach(function(inputElement) {
+        inputElement.addEventListener("input", function() {
+            checkInputValidityForm2(form, inputElement);
+
+            toggleForm2(inputList, buttonElement);
+        });
+    });
+};
+
+const enableValidationForm2 = () => {
+    const formList = Array.from(document.querySelectorAll(".form__form"));
+
+    formList.forEach((form) => {
+        form.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+        });
+        setEventListenersForm2(form);
+    });
+};
+
+enableValidationForm2({
+    formSelector: ".form__form",
+    inputSelector: ".form__input",
+    submitButtonSelector: ".form__button-form",
+    inactiveButtonClass: "form__button-form_inactive",
+    inputErrorClass: "form__active-error",
+    errorClass: "form__valid"
+});
