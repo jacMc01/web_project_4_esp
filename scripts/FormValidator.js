d3722403d8a481da36fe2668b77b48fdaf6664c8
Jacqueline Mckay H <jacquelinemc@outlook.es>
2022-07-19 14:46:05 -0500

    feat: validation forms and Card using class

---
+++ scripts/FormValidator.js
@@ -0,0 +1,110 @@
+
+export default class FormValidator {
+    constructor(config) {
+
+        this._form = document.forms[config.formName]
+        this._type = config.type;
+
+    }
+
+    enableValidation() {
+        this._form.addEventListener("submit", (evt) => {
+            evt.preventDefault()
+        });
+
+        this._setEventListeners();
+
+        // funciones para el form 2 de las tarjetas
+        // formList = Array.from(document.querySelectorAll(".form__form"));
+        // formList.forEach((form) => {
+        //     form.addEventListener("submit", (evt) => {
+        //         evt.preventDefault();
+        //     });
+        //     this._setEventListeners(form, "card");
+        // });
+    }
+
+     static _checkInputValidity(form, inputElement) {
+
+        const errorElement = form.querySelector(`.${inputElement.id}-error`);
+
+        if (!inputElement.validity.valid) {
+
+            inputElement.classList.add("popup__valid");
+            errorElement.textContent = inputElement.validationMessage;
+            errorElement.classList.remove("popup__active-error")
+
+        } else {
+
+            inputElement.classList.remove("popup__valid");
+            errorElement.classList.add("popup__active-error");
+            errorElement.textContent = "";
+        }
+    }
+
+    static _hasInvalidInput(inputList) {
+        return inputList.some((inputElement) => {
+            return !inputElement.validity.valid;
+        });
+    }
+
+    static _toggle(type, inputList, buttonElement) {
+
+        if (type === "profile") {
+
+            if (FormValidator._hasInvalidInput(inputList)) {
+                buttonElement.classList.add("popup__button-form_inactive");
+                buttonElement.disabled = true;
+            } else {
+                buttonElement.classList.remove("popup__button-form_inactive");
+                buttonElement.disabled = false;
+            }
+
+        } else if (type === "card") {
+
+
+            if (FormValidator._hasInvalidInput(inputList)) {
+                buttonElement.classList.add("form__button-form_inactive");
+                buttonElement.disabled = true;
+            } else {
+                buttonElement.classList.remove("form__button-form_inactive");
+                buttonElement.disabled = false;
+            }
+        }
+    };
+
+    _setEventListeners() {
+
+        let inputList = []
+        let buttonElement = null
+
+        if (this._type === "profile") {
+
+            inputList = Array.from(this._form.querySelectorAll(".popup__input"));
+            buttonElement = this._form.querySelector(".popup__button-form");
+
+        } else if (this._type === "card") {
+            // funciones para el form 2
+
+            inputList = Array.from(this._form.querySelectorAll(".form__input"));
+            buttonElement = this._form.querySelector(".form__button-form");
+        }
+
+        const form = this._form
+        const type = this._type;
+
+        FormValidator._toggle(type, inputList, buttonElement);
+
+        inputList.forEach(function (inputElement) {
+
+            inputElement.addEventListener("input", function () {
+
+                FormValidator._checkInputValidity(form, inputElement);
+                FormValidator._toggle(type, inputList, buttonElement);
+
+            });
+        });
+
+    };
+
+}
---
+++ scripts/index.js
@@ -0,0 +1,96 @@
+import Card from "./card.js";
+import FormValidator from "./formValidator.js";
+import * as utils from "./utils.js";
+
+const elements = document.querySelector(".elements");
+
+// funcion para agregar las cards.
+// se usa para hacer el llenado inicial y agregar con el boton
+const addCard = (url, description) => {
+    const cardData = {
+        name: description,
+        link: url
+    }
+
+    const card = new Card(cardData, ".elements__element");
+    const cardElement = card.generateCard();
+
+    elements.appendChild(cardElement);
+}
+
+// agregar listener al boton que lanza el form para agregar cards
+const add_card_button = document.querySelector('.profile__btn-image');
+add_card_button.addEventListener("click", openFormImages)
+
+// set de codigo para agregar una card individual.
+///////////////////////////////////////////////////////////////
+function event_add_card(e) {
+
+    e.preventDefault();
+
+    addCard(e.target.form.elements.form__about.value, e.target.form.elements.form__name.value);
+
+    e.target.form.elements.form__about.value = "";
+    e.target.form.elements.form__name.value = "";
+
+    setTimeout(closeFormImages, 175)
+}
+document.querySelector('.form__button-form').addEventListener("click", event_add_card)
+    ///////////////////////////////////////////////////////////////
+
+// set de codigo para editar el perfil
+///////////////////////////////////////////////////////////////
+function handleProfileFormSubmit(evt) {
+
+    evt.preventDefault();
+
+    const profile__name = document.querySelector('.profile__name');
+    const profile__about = document.querySelector('.profile__about');
+
+    profile__name.textContent = evt.target.form.elements.popup__name.value;
+    profile__about.textContent = evt.target.form.elements.popup__about.value;
+
+    evt.target.form.elements.popup__name.value = "";
+    evt.target.form.elements.popup__about.value = "";
+
+    setTimeout(closeForm, 175)
+
+}
+document.querySelector(".popup__button-form").addEventListener("click", handleProfileFormSubmit)
+    //////////////////////////////////////////////////////////
+
+// set de codigo de los botones
+function openForm() {
+    document.querySelector(".popup").style.display = "block";
+    // llamar la clase de validacion de los inputs
+
+    const config = {
+        formName: "popup__form",
+        type: "profile"
+    }
+    const validationObject = new FormValidator(config);
+    validationObject.enableValidation();
+
+}
+export function closeForm() {
+    document.querySelector(".popup").style.display = "none";
+}
+function openFormImages() {
+    document.querySelector(".form").style.display = "block";
+
+    const config = {
+        formName: "form__form",
+        type: "card"
+    }
+    const validationObject = new FormValidator(config);
+    validationObject.enableValidation();
+}
+export function closeFormImages() {
+    document.querySelector(".form").style.display = "none";
+
+}
+document.querySelector('.profile__button-person').addEventListener("click", openForm)
+document.querySelector('.popup__button-close').addEventListener("click", closeForm)
+document.querySelector('.form__button').addEventListener("click", closeFormImages)
+
+utils.setUpListeners()
